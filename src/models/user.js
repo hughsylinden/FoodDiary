213fd21a5fc542265b123916f950ffceb3106f0e
Hugh Kristiansen <kaarelinden@hotmail.com>
2022-01-03 17:32:46 +0000

    added validation for duplicate user names

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,10 @@
 module.exports = (connection, DataTypes) => {
-  const schema = {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
+  const schema = {  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+    },
+    password: DataTypes.STRING,    
   };
 
   const UserModel = connection.define('User', schema);
