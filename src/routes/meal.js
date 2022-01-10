1a1d9f4d9661e841ad8ec585fc96e1086bc22345
hughsylinden <kaarelinden@hotmail.com>
2022-01-10 23:21:15 +0100

    added new meal controllers routes

--- src/routes/meal.js
+++ src/routes/meal.js
@@ -7,6 +7,8 @@ MealRouter.post('/', MealController.create);
 MealRouter.get('/', MealController.read);
 MealRouter.get('/search/:FoodDiaryId', MealController.readByFoodDiaryId);
 MealRouter.post('/searchbydate/', MealController.readByFoodDiaryIdAndDate);
+MealRouter.post('/searchbymonth/', MealController.readByFoodDiaryIdAndMonth);
+MealRouter.post('/searchbyyear/', MealController.getAllMealsInYear);
 MealRouter.get('/:id', MealController.readOne);
 MealRouter.patch('/:id', MealController.update);
 MealRouter.delete('/:id', MealController.destroy);
