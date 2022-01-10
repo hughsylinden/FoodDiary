df02ae7334700208794f1100906c1c248f1d5aaa
hughsylinden <kaarelinden@hotmail.com>
2022-01-10 23:21:00 +0100

    updated meal table to store daily calorie target

--- src/models/fooddiary.js
+++ src/models/fooddiary.js
@@ -1,6 +1,10 @@
 module.exports = (connection, DataTypes) => {
   const schema = {
     name: DataTypes.STRING,
+    dailyCalorieTarget: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   };
 
   const FoodDiaryModel = connection.define('FoodDiary', schema);
