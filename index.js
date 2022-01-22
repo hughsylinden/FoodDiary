8f9774003ac287d7e3fc87ec34fae1adb2380605
hughsylinden <kaarelinden@hotmail.com>
2022-01-22 17:07:46 +0100

    heroku script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./src/app');
 
 const APP_PORT = process.env.PORT || 4000;
+const DB_PORT = process.env.DB_HOST
 
 app.listen(APP_PORT, () => {
   console.log(`App is listening on port ${APP_PORT}`)
+  console.log(`App is listening on port ${DB_PORT}`)
 })
